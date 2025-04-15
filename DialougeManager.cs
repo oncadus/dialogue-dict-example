b0e6d14e53360a6b308de7af2ce0e7e6079fb974
oncadus <oncadus@proton.me>
2025-04-15 00:47:09 -0600

    init

---
+++ DialougeManager.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class DialougeManager : Panel
+{
+    Godot.Collections.Dictionary dialouge = new Godot.Collections.Dictionary{
+        {"Gork", "lowk yall r sleeping on ultraball anal beads"},
+        {"David", "???????what"}
+    };
+}
